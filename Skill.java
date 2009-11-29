3d25b367e7c75d40ce3b6904f5c58e11fd32a6cd
Jordan Bach <jordanbach@gmail.com>
2009-11-29 15:01:55 -0600

    Added getters/setters to Skill class

--- Skill.java
+++ Skill.java
@@ -1,6 +1,37 @@
 class Skill
 {
 	private String skillName;
+	private double weight;
 	private int rating;
+
+	public String getSkillName()
+	{
+		return skillName;
+	}
+
+	public void setSkillName(String value)
+	{
+		skillName = value;
+	}
+
+	public double getWeight()
+	{
+		return weight;
+	}
+
+	public void setWeight(double value)
+	{
+		weight = value;
+	}
+
+	public int getRating()
+	{
+		return rating;
+	}
+
+	public void setRating(int value)
+	{
+		rating = value;
+	}
 }
 
