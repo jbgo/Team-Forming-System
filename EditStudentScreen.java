c890666cc713f53f09f98eb2e5f571e3d4df0369
Jordan Bach <jordanbach@gmail.com>
2009-12-05 16:17:07 -0600

    Can now enter student info manually (except for skillset)

--- EditStudentScreen.java
+++ EditStudentScreen.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.regex.*;
 
 public class EditStudentScreen extends Screen implements ActionListener
 {
 	Student student;
 	Screen parentScreen;
+	TFSFrame mainFrame;
 
 	JTextField lastNameField = new JTextField();
 	JTextField firstNameField = new JTextField();
@@ -20,6 +22,7 @@ public class EditStudentScreen extends Screen implements ActionListener
 
 	public EditStudentScreen(Screen parent, Student st, boolean isNewStudent)
 	{
+		mainFrame = TFSFrame.getInstance();
 		parentScreen = parent;
 		student = st;
 		initComponents(isNewStudent);
@@ -38,20 +41,76 @@ public class EditStudentScreen extends Screen implements ActionListener
 	public void actionPerformed(ActionEvent e)
 	{
 		Object source = e.getSource();
-		TFSFrame mainFrame = TFSFrame.getInstance();
 
-		if (source == updateButton) {
-			student.setFirstName(firstNameField.getText());
-			student.setLastName(lastNameField.getText());
-			student.setUtdEmail(emailField.getText());
-
+		if (source == updateButton && saveStudentData()) {
 			Project proj = mainFrame.getCurrentProject();
 			proj.addStudent(student);
 			parentScreen.reloadData(proj);
+			mainFrame.setScreen(parentScreen);
 		} else if (source == cancelButton) {
+			mainFrame.setScreen(parentScreen);
+		}
+	}
+
+	private boolean saveStudentData()
+	{
+		String errorMessages = "";
+		boolean isValid = true;
+
+		String firstName = firstNameField.getText().trim();
+		if (firstName.isEmpty()) {
+			errorMessages += "Enter a first name.\n";
+			isValid = false;
 		}
 
-		mainFrame.setScreen(parentScreen);
+		String lastName = lastNameField.getText().trim();
+		if (lastName.isEmpty()) {
+			errorMessages += "Enter a last name.\n";
+			isValid = false;
+		}
+
+		String email = emailField.getText().trim().toLowerCase();
+		if (!Pattern.matches("^[a-z]{3}\\d{6}@utdallas\\.edu", email)) {
+			errorMessages += "Use a valid UTD email address (e.g. abc123000@utdallas.edu)\n";
+			isValid = false;
+		}
+
+		String phone = phoneField.getText().trim();
+		if (!Pattern.matches("^\\d{10,}$", phone)) {
+			errorMessages += "Phone numbers may only contain numbers and must be at least 10 digits long.\n";
+			isValid = false;
+		}
+
+		String txt = GPAField.getText().trim();
+		boolean isGpaValid = true;
+		double gpa = 0.0;
+		try {
+			gpa = Double.parseDouble(txt);
+			if (gpa < 0.0 || gpa > 4.0) {
+				isGpaValid = false;
+			}
+		} catch (NumberFormatException ex) {
+			isGpaValid = false;
+		} finally {
+			if (!isGpaValid) {
+				errorMessages += "A GPA must be between 0.0 and 4.0.\n";
+				isValid = false;
+			}
+		}
+
+		// TODO: get skillset
+
+		if (!isValid) {
+			mainFrame.showError(errorMessages);
+		} else {
+			student.setFirstName(firstName);
+			student.setLastName(lastName);
+			student.setUtdEmail(email);
+			student.setPhoneNumber(phone);
+			student.setGPA(gpa);
+		}
+
+		return isValid;
 	}
 
 	private void initComponents(boolean isNewStudent)
